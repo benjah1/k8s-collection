67759eabae26f9791678f767ecb3f6233fcfa2c2
benjah1 <benjaminhuang1@gmail.com>
2020-07-28 23:14:54 +0000

    create cluster_up.sh for kube-router

---
+++ k8s-cluster/cluster_up.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+RES=$(kubectl --context kind-kind get node 2>/dev/null 1>/dev/null)
+
+if [ "$?" == "0" ]
+then 
+	echo "The cluster is already running!!!"
+	exit 1
+fi
+
+RES=$(docker image ls | egrep "kind-node +v1")
+
+
+if [ "$?" == "1" ]
+then
+
+	echo ""
+	echo "Build image kind-node"
+	docker build -t kind-node:v1 -f kind-node.dockerfile .
+fi
+
+echo ""
+echo "Create cluster"
+kind create cluster --config ./kind.yaml
+
+echo ""
+echo "Deploy kube-router"
+MASTER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
+
+APISERVER=https://${MASTER_IP}:6443
+
+cat kube-router.yaml | \
+	sed "s|%APISERVER%|${APISERVER}|g" | \
+  kubectl apply -f -
