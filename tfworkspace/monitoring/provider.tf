e3962070eb4391cd58324b8cb16c9780ded9ad6f
benjah1 <benjaminhuang1@gmail.com>
2020-09-29 01:42:38 +0000

    update terraform k8s plugin

--- tfworkspace/monitoring/provider.tf
+++ tfworkspace/monitoring/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      versions = ["0.1"]
-      source = "example.com/myorg/kubernetes"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_context   = "kind-kind"
 }
